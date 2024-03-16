8d2141bced7e9a18e151a895b6343d8b14a1f820
Muhammed Mert Bek <148920150+mertbek10@users.noreply.github.com>
2024-03-17 00:18:19 +0300

    second
    sonsuz döngüde

--- convert binary to decimal/binary_to_decimal.cpp
+++ convert binary to decimal/binary_to_decimal.cpp
@@ -2,25 +2,42 @@
 //convert to decimal from binary
 
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
-    int sayi,taban=1,decimal,sayac,result=0;
 
-cout<<"enter your binary number : ";
-cin>>sayi;
+    string exit;
+    int sayi,taban=1,decimal,sayac,result=0,a=1;
 
-while(sayi>=1)
-{
-    decimal = (sayi%10) * taban; //girilen sayisinin her basamağını 2 nin üstleriyle (0 dan başlayıp artırarak) çarp
-    sayi = sayi / 10; //bir basamak eksilt
-    result = result + decimal;
-    taban = taban*2; //2 nin üstlerini artır
-}
-cout<<"decimal number : "<< result;
+    while(a==1)
+    {
+        cout<<"enter your binary number : ";
+        cin>>sayi;
 
+           while(sayi>=1)
+           {
+             decimal = (sayi%10) * taban; //girilen sayisinin her basamağını 2 nin üstleriyle (0 dan başlayıp artırarak) çarp
+             sayi = sayi / 10; //bir basamak eksilt
+             result = result + decimal;
+             taban = taban*2; //2 nin üstlerini artır
+           }
 
+        cout<<"decimal number : "<< result<<endl;
 
+        cout<<"if u wanna continue pres any key or type exit : ";
+        cin>>exit;
+
+        if(exit == "exit")
+        { 
+        a=0;
+        }
+//eger devam ediyorsa degiskenleri resetle
+     result=0;
+     decimal=0;
+     taban=1;
+
+    }
 
     return 0;
 }
--- convert decimal to binary/decimal_to_binary.cpp
+++ convert decimal to binary/decimal_to_binary.cpp
@@ -2,36 +2,68 @@
 //convert to binary from decimal
 
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
- int sayi,dizi[100],i=0,sayac=0;
+ int sayi,dizi[100],i=0,sayac=0,a=1;
+ string exit;
 
-cout<<"enter your decimal number : ";
-cin>>sayi;
+ 
+while(a==1)
+    {
+       cout<<"enter your decimal number : ";
+       cin>>sayi;
 
-while(sayi>1)
-{
-    dizi[i]=sayi%2; //sayinin 2 ye göre modunu diziye eşitle
-    i++; 
-    sayi=sayi/2; //girilen sayiyi daima 2 ye böl
+    //2 ve 3 sayilari algoritmanın kilit sayilari oldugundan extra koşullarla kontrol ediliyor
+    //eger kullanıcı 2 ve 3 un binary degerlerini isterse hic donguye girmeden degerleri yazdir
+    if(sayi==2)
+      {
+        cout<<"binary number : "<<10;
+        break;
+      }
 
-       if( sayi==2 || sayi==3 ) //sayi en son ya 2 ya da 3 kalacaktır ve döngüden çık
-        {
-           break;
+    else if(sayi==3)
+       {
+        cout<<"binary number : "<<11;
+        break;
+       }
+
+      while(sayi>1)
+       {
+          dizi[i]=sayi%2; //sayinin 2 ye göre modunu diziye eşitle
+          i++; 
+          sayi=sayi/2; //girilen sayiyi daima 2 ye böl
+
+             if( sayi==2 || sayi==3 ) //sayi en son ya 2 ya da 3 kalacaktır ve döngüden çık
+              {
+                 break;
+              }
+           sayac++; // diziyi yazdırırken nerden başlayacağını belirlemek için sayac kullan
         }
-     sayac++; // diziyi yazdırırken nerden başlayacağını belirlemek için sayac kullan
-}
 
-cout<<"binary number : ";
+        cout<<"binary number : ";
 
-cout<< sayi/2<< sayi%2; //3 veya 2 sayisinin 2 ye bölümünden başla arkasından aynı sayinin 2 ye göre modunu yazdir
+        cout<< sayi/2<< sayi%2; //3 veya 2 sayisinin 2 ye bölümünden başla arkasından aynı sayinin 2 ye göre modunu yazdir
 
-for(i=sayac ; i>=0 ; i--)
-{
-    cout<<dizi[i];//while döngüsünde aldığımız modları yazdır
-}
+        for(i=sayac ; i>=0 ; i--)
+         {    
+         cout<<dizi[i];//while döngüsünde aldığımız modları yazdır
+         }
+        cout<<endl;
+        cout<<"if u wanna continue pres any key or type exit : ";
+        cin>>exit;
 
+        if(exit == "exit")
+        { 
+        a=0;
+        }
+
+        i=0;
+        sayac=0;
+
+    }
+    
     return 0;
 }
 
